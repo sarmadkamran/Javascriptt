cfa30f7ed291022b4e5ffb3ea41ab95dc0f78db5
sarmadkamran <kamransarmad9@gmail.com>
2020-03-11 15:09:03 +0500

    J S

---
+++ index.js
@@ -0,0 +1,76 @@
+//var percentage =prompt('your percentage')
+//if(percentage >=50 && percentage <60)
+//{
+//    alert('fail')
+//}
+//else if(percentage >= 60 && percentage <=100)
+//else 
+//{
+ //   alert ('pass')
+//}
+
+
+//var percentage =prompt('your percentage')
+//if(percentage >70 && percentage<=80 )
+//{alert('c')
+//}
+//else if(percentage >80 && percentage <=90 )
+//{
+  //  alert('b')
+//}
+//else if(percentage >90 && percentage<=100)
+//{
+  //  alert('a')
+//}
+//else{
+//    ('fail ')
+
+
+// var percentage =prompt('your percentage')
+
+// if(percentage >90 && percentage <=100)
+// {
+//     alert('A+')
+// }
+// else if(percentage >80 && percentage <=90 )
+// {
+//     alert('A')
+// }
+// else if( percentage >70 && percentage <=80)
+// {
+//     alert('B')
+// }
+// else if(percentage >60 && percentage <=70)
+// {
+//     alert('C')
+// }
+// else if(percentage>50 && percentage<=60)
+// {
+//     alert('D')
+// }
+// else{
+//     alert('fail')
+// }
+//  var students = ['sarmad' , 'imran' , 'saeed' , 'kashan' , 'najaf' , 'hamza' , 'saad' , 'shan']
+// console.log(students)
+// students[2]= 'sir bilal'
+// console.log(students)
+// students[8]= 'rizwan'
+// console.log(students)
+
+var students = ['sarmad' , 'imran' , 'saeed' , 'kashan' , 'najaf' , 'hamza' , 'saad' , 'shan']
+console.log(students)
+students.push('shayan')
+console.log(students)
+students.pop()
+console.log(students)
+students.shift()
+console.log(students)
+students.unshift('sarmad')
+console.log(students)
+students.splice(3 , 0 , 'fareed')
+console.log(students)
+students.splice(4 , 2 , 'umair')
+console.log(students)
+students.splice(4 , 0 , 'kashan' , 'najaf')
+console.log(students)
